257570acbc0b9f86ab887e9adc93720eacca62e4
ahaduzzaman5503 <ahaduzzaman5503@gmail.com>
2023-05-20 15:49:04 +0600

    post data on server

--- index.js
+++ index.js
@@ -1,10 +1,58 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
-const cors = require('cors')
-const toycars = require('./toycar.json')
-const port = 5000;
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors());
+app.use(express.json());
+
+
+const uri = "mongodb+srv://ahaduzzaman45503:d4IbTfNI8nR5uH0V@cluster0.qzwnegb.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+
+    await client.connect();
+
+    const database = client.db("toyDB");
+    const carCollection = database.collection("toyData");
+
+    app.post('/toycardata', async(req, res) => {
+      const cardata = req.body;
+      console.log('new car data', cardata);
+      const result = await carCollection.insertOne(cardata);
+      res.send(result);
+    } );
+
+
+
+
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+
 
 app.get('/', (req, res) => {
   res.send('Server is Running Now')
