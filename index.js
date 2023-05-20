c87fdd84fb7a1a36d2611941afb4c5e33525ad5b
ahaduzzaman5503 <ahaduzzaman5503@gmail.com>
2023-05-20 08:00:00 +0600

    picture data added on json file

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
+const toycars = require('./toycar.json')
 const port = 5000;
 
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Server is Running Now')
 });
+
+app.get('/toy', (req, res) => {
+  res.send(toycars)
+});
+
+app.get('/toy/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const toy = toycars.find(toy => toy.id === id) || {};
+  res.send(toy)
+})
 
 app.listen(port, () => {
   console.log(`Server side running on this port ${port}`)
