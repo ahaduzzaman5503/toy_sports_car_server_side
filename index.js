8a4ac9a2f4cff25f0b0044a2d3f8da00d29c22e4
ahaduzzaman5503 <ahaduzzaman5503@gmail.com>
2023-05-20 23:08:51 +0600

    some update

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 app.use(cors());
 app.use(express.json());
 
-
 const uri = "mongodb+srv://ahaduzzaman45503:d4IbTfNI8nR5uH0V@cluster0.qzwnegb.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -21,8 +20,20 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+    const alltoydatabase = client.db("alltoycarDB");
+    const alltoyCollection = alltoydatabase.collection("alltoycardata");
 
-    await client.connect();
+    app.get('/toy', async(req, res) => {
+      const cursor = alltoyCollection.find();
+      const result = await cursor.toArray();
+      res.send(result)
+    });
+
+    app.get('/toy/:id', (req, res) => {
+      const id = parseInt(req.params.id);
+      const toy = alltoyCollection.find(toy => toy.id === id) || {};
+      res.send(toy)
+    })
 
     const database = client.db("toyDB");
     const carCollection = database.collection("toyData");
@@ -38,41 +49,18 @@ async function run() {
       console.log('new car data', cardata);
       const result = await carCollection.insertOne(cardata);
       res.send(result);
-    } );
+    });
 
-
-
-
-
-
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
   }
 }
 run().catch(console.dir);
 
 
-
-
-
-
-
 app.get('/', (req, res) => {
   res.send('Server is Running Now')
 });
-
-app.get('/toy', (req, res) => {
-  res.send(toycars)
-});
-
-app.get('/toy/:id', (req, res) => {
-  const id = parseInt(req.params.id);
-  const toy = toycars.find(toy => toy.id === id) || {};
-  res.send(toy)
-})
 
 app.listen(port, () => {
   console.log(`Server side running on this port ${port}`)
